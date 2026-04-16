a548a9a42cba8b8528fb23029eb9ff54047fd9d3
phw0224 <phw0224@quazar.co.kr>
2026-04-16 17:42:46 +0900

    github App

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kk8u5qv5ji8"
+    "revision": "0.p93sh15k328"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
